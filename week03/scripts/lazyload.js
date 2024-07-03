80e9a9c54d5220887e69180037e459cd3dcfd36a
EuChat <grkchataika@gmail.com>
2024-07-03 20:55:39 +0200

    lazy loading

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,5 @@
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
